40fdf0b2d9be6eac0ce602752235b27b55b8c98a
ksgunasekera <ksgunasekera@gmail.com>
2017-09-10 10:11:51 +0530

    Add login page

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express=require('express');
 const app=express();
 const index=require('./routes/index');
 const users=require('./routes/users');
+const login=require('./routes/login');
 
 app.use('/',index);
 app.use('/user',users.r);
+app.use('/login',login);
 
 app.listen(3000);
